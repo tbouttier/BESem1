e0bd3f0ba6822adedb9f5f03db3d2c58643534dc
Teo Bouttier <bouttier.teo@gmail.com>
2023-08-31 11:32:14 +0200

    Ajout fonction getCardImg

--- fonctions.py
+++ fonctions.py
@@ -1,2 +1,13 @@
+import csv
 
-def afficherCartes()
+def getCardImg(fic,valeur):
+    """
+    Renvoi le filename de l'image en fonction de la valeur de la carte
+    """
+    with open(fic, 'r') as fichier:
+        cardDict = csv.DictReader(fichier, delimiter=',')
+        for card in cardDict:
+            if card['Valeur'] == valeur:
+                imgName = card.get('file')
+    
+    return imgName

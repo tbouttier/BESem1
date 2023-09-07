f8805f908aac8e92f77a051b0f4259a1cbf86fda
Téo Bouttier <bouttier.teo@gmail.com>
2023-09-07 17:32:16 +0200

    Essai affichage par div

---
+++ apptest.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Sample list of items (you can replace this with your data)
+items = [
+    ["Item 1", "Item 2", "Item 3"],
+    ["Item 4", "Item 5", "Item 6"],
+    ["Item 7", "Item 8", "Item 9"],
+]
+
+@app.route('/')
+def item_list():
+    return render_template('index.html', items=items)
+
+if __name__ == '__main__':
+    app.run(debug=True)

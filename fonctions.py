d2513428ae502a7e9423ce8782f79fab14731a1e
Téo Bouttier <bouttier.teo@gmail.com>
2023-09-15 10:51:39 +0200

    Teo avant yann (#5)
    * Ajout de la fonction getCardValue
    
    * Ajout fonction cardPlayed
    
    * Fonctions cardPlayed et init_game
    
    * Commit for merge with yann

--- fonctions.py
+++ fonctions.py
@@ -1,15 +1,62 @@
 import csv, random, ast
+from enum import IntEnum
+
+def init_game():
+    """
+    Initialise une nouvelle partie
+
+    :return mainJ1,mainJ2,cache1,cache2 (tuple) : Les mains des joueurs et les cartes cachés
+    """
+
+    card_file ='static/cards/cards.csv'
+
+    donne = distribCards()
+    mainJ1 = []
+    mainJ2 = []
+    cacheJ1 = []
+    cacheJ2 = []
+
+    for card in donne[0]:
+        mainJ1.append({'valeur':card, 'image':getCardImg(card_file, str(card))})
+    for card in donne[1]:
+        mainJ2.append(({'valeur':card, 'image':getCardImg(card_file, str(card))}))
+    for card in donne[2]:
+        cacheJ1.append({'valeur':card, 'image':getCardImg(card_file, str(card))})
+    for card in donne[3]:
+        cacheJ2.append({'valeur':card, 'image':getCardImg(card_file, str(card))})
+
+    return mainJ1,mainJ2,cacheJ1,cacheJ2
 
 def checkBelote(main):
     """
     Vérifie si le joueur détient une belote dans sa main
     """
 
+def cardPlayed(tour,mainJoueur,cacheJoueur,jeu,carte_jouee):
+    """Joue la carte choisie par le joueur
+
+    Args:
+        joueur (int): numéro du joueur
+        donne (_type_): 
+        carte_jouee (_type_): ImgName de la carte jouée
+    """
+    if type(carte_jouee) != int:
+        raise TypeError(f"carte_jouee doit être un int pas {type(carte_jouee)}")
+
+    jeu[carte_jouee] = mainJoueur[carte_jouee]
+    mainJoueur[carte_jouee] = cacheJoueur[carte_jouee]
+    cacheJoueur[carte_jouee] = 'carte_0.png'
+
+    return tour+1
+
+
 def distribCards():
     """
     Crée une liste des valeurs des cartes de 1 à 32
     Mélange et distribue les cartes aux joueurs
-    :return tuple: 2 listes main_J1, main_J2
+    
+    
+    :return (tuple): 4 listes main_J1, main_J2, cacheJ1, cacheJ2
     """
     cards = []
     for value in range(1, 33):
@@ -39,7 +86,22 @@ def distribCards():
 
     return vis1,vis2,cache1,cache2
 
-def getCardImg(fic: str, valeur: int):
+def getCardValue(fic,img_name):
+    """
+    Renvoi la valeur de la carte en fonction du filename de l'image
+    :param str fic: fichier .csv contenant les valeurs des cartes et les noms des images
+    :param string img_name: Nom du fichier img de la carte recherchée
+    :return str valeur
+    """
+    with open(fic, 'r') as fichier:
+        cardDict = csv.DictReader(fichier, delimiter=',')
+        for card in cardDict:
+            if card['file'] == img_name:
+                valeur: str = card.get('Valeur')
+
+    return valeur
+
+def getCardImg(fic: str, valeur: str):
     """
     Renvoi le filename de l'image en fonction de la valeur de la carte
     :param str fic: fichier .csv contenant les valeurs des cartes et les noms des images
@@ -57,8 +119,8 @@ def getCardImg(fic: str, valeur: int):
 def getCardColor(fic: str,valeur: str):
     """
     Renvoi la couleur d'une carte en fonction de sa valeur
-    :param valeur str : Utilise la valeur de la carte pour rechercher la couleur (doit être convertie en str)
-    :return str couleur: Renvoit la couleur de la carte entrée
+    :param valeur (str) : Utilise la valeur de la carte pour rechercher la couleur (doit être convertie en str)
+    :return couleur (str): Renvoit la couleur de la carte entrée
     """
     with open(fic, 'r') as fichier:
         cardDict = csv.DictReader(fichier, delimiter=',')
@@ -72,7 +134,7 @@ def getCardColor(fic: str,valeur: str):
 def startingPlayer():
     """
     Determine le joueur commençant la partie
-    :return int : Renvoit le numéro du joueur d'atout 
+    :return int : Renvoit le numéro du joueur commençant la partie
     """
     joueur = random.randint(0,1)
     return joueur
--- run.py
+++ run.py
@@ -4,33 +4,23 @@ import fonctions
 card_file = 'static/cards/cards.csv'
 
 app = Flask(__name__)
-mainJ1_IMG = []
-mainJ2_IMG = []
-cacheJ1_IMG = []
-cacheJ2_IMG = []
 
-donne = fonctions.distribCards()
-mainJ1= donne[0]
-mainJ2= donne[1]
-cacheJ1 = donne[2]
-cacheJ2 = donne[3]
-mt_line = ["carte_0.png","carte_0.png","carte_0.png","carte_0.png","carte_0.png","carte_0.png","carte_0.png","carte_0.png"]
+donne = fonctions.init_game()
 
-for card in mainJ1:
-        mainJ1_IMG.append(fonctions.getCardImg(card_file,str(card)))
-for card in mainJ2:
-        mainJ2_IMG.append(fonctions.getCardImg(card_file,str(card)))
-for card in cacheJ1:
-        cacheJ1_IMG.append("dos.jpg")
-for card in cacheJ2:
-    cacheJ2_IMG.append("dos.jpg")
+tapis1 = ["carte_0.png","carte_0.png","carte_0.png","carte_0.png","carte_0.png","carte_0.png","carte_0.png","carte_0.png"]
+tapis2 = ["carte_0.png","carte_0.png","carte_0.png","carte_0.png","carte_0.png","carte_0.png","carte_0.png","carte_0.png"]
 
-donneIMG = (mainJ1_IMG, cacheJ1_IMG,mt_line,cacheJ2_IMG,mainJ2_IMG)
+
+donneIMG = (donne[0], donne[2],tapis1,tapis2,donne[3],donne[1])
 
 @app.route('/')
 def index():
-    #for i in range(0,7):
-        #table_liste = [mainJ1_IMG[i],cacheJ1_IMG[i],None,cacheJ2_IMG[i],mainJ2_IMG[i]]
+    
+    return render_template('index.html')
+
+@app.route('/game')
+def game():
+
 
     return render_template('belote.html', donne = donneIMG)
 

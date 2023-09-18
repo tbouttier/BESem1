a817af080337bf07b11c6d17c2105581a252f847
Téo Bouttier <bouttier.teo@gmail.com>
2023-09-19 01:52:13 +0200

    Update Main (#8)
    * Card played implement
    
    * cardPlayed fix + DocString update
    
    * Infinite recursion fix
    
    * Condition loops minimify
    
    * Tour, game init (wip), atout, playCard J1 J2
    
    * added testfile to gitignore
    
    * gitignore

--- run.py
+++ run.py
@@ -1,22 +1,31 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, redirect
 import fonctions
 import random
 
 card_file = 'static/cards/cards.csv'
 
-app = Flask(__name__)
+app = Flask('__main__')
 mainJ1_IMG = []
 mainJ2_IMG = []
 cacheJ1_IMG = []
 cacheJ2_IMG = []
 
-donne = fonctions.init_game()
+tapis1 = []
+tapis2 = []
 
-tapis1 = ["carte_0.png","carte_0.png","carte_0.png","carte_0.png","carte_0.png","carte_0.png","carte_0.png","carte_0.png"]
-tapis2 = ["carte_0.png","carte_0.png","carte_0.png","carte_0.png","carte_0.png","carte_0.png","carte_0.png","carte_0.png"]
+mains = fonctions.init_game()
+donne = (mains[0],mains[2],tapis1,tapis2,mains[3],mains[1])
+
+#Détermine qui joue
+tour = fonctions.startingPlayer()
 
 
-donneIMG = (donne[0], donne[2],tapis1,tapis2,donne[3],donne[1])
+atout = fonctions.getCardColor(card_file,str(donne[tour][-1]['valeur']))
+
+
+for i in range(8):
+    tapis1.append({'valeur':None, 'image':"carte_0.png"})
+    tapis2.append({'valeur':None, 'image':"carte_0.png"})
 
 @app.route('/')
 def index():
@@ -26,12 +35,26 @@ def index():
 @app.route('/game')
 def game():
 
-    donneIMG = (mainJ1_IMG, cacheJ1_IMG,mt_line,cacheJ2_IMG,mainJ2_IMG)
-    return render_template('belote.html', donne = donneIMG)
+    return render_template('belote.html', donne = donne, tour = tour, atout =atout)
+
+@app.route('/j1/<card>')
+def jeu_j1(card):
+    fonctions.cardPlayed(0,donne[0],donne[1],donne[2],donne[0][int(card)-1]['valeur'],int(card))
+    global tour
+    tour+=1
+    
+    return redirect('/game')
 
 
+@app.route('/j2/<card>')
+def jeu_j2(card):
+    fonctions.cardPlayed(0,donne[5],donne[4],donne[3],donne[5][int(card)-1]['valeur'],int(card))
+    global tour
+    tour-=1
+    
+    return redirect('/game')
+    
 
 if __name__=='__main__':
     app.secret_key = "teobubu"
     app.run(debug='True')
-

fc006207f6ae4818fbbce0bd829ce1c8677e7ef5
Téo Bouttier <bouttier.teo@gmail.com>
2023-09-25 00:49:21 +0200

    loadGame associated functions

--- run.py
+++ run.py
@@ -1,13 +1,19 @@
-from flask import Flask, render_template, request, session, redirect
+import os
+from flask import Flask, render_template, request, flash, redirect, send_file
+from werkzeug.utils import secure_filename
 import fonctions
 
+UPLOAD_FOLDER = 'saves/'
+ALLOWED_EXTENSIONS = {'bel'}
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 card_file = 'static/cards/cards.csv'
 
 app = Flask('__main__')
-
-#Détermine qui joue
-
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def index():
@@ -16,14 +22,14 @@ def index():
 
 @app.route('/game')
 def game():
-    global joueur
     fonctions.checkTapis(donne[2][0],donne[3][0],atout,score,joueur,pli)
+    if pli[0] < 16:
+        fonctions.saveGame('saves/gameData.bel',donne,pli,joueur,atout,score)
     return render_template('game.html', donne = donne, joueur = joueur[0], atout = atout,score=score,pli = pli[0])
 
 @app.route('/j1/<card>')
 def jeu_j1(card):
     fonctions.cardPlayed(donne[0],donne[1],donne[2],donne[0][int(card)-1]['valeur'],int(card))
-    global joueur
     joueur[0]+=1
     
     return redirect('/game')
@@ -32,11 +38,38 @@ def jeu_j1(card):
 @app.route('/j2/<card>')
 def jeu_j2(card):
     fonctions.cardPlayed(donne[5],donne[4],donne[3],donne[5][int(card)-1]['valeur'],int(card))
-    global joueur
     joueur[0]-=1
     
     return redirect('/game')
 
+@app.route('/loadGame', methods=['GET', 'POST'])
+def loadGame():
+    if request.method == 'POST':
+        if 'save' not in request.files:
+            flash('Fichier innaproprié')
+            
+            return redirect('/loadGame')
+        file = request.files['save']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect('/loadGame')
+        
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        
+            global donne,atout,joueur,score,pli
+            gameData = fonctions.loadGame(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            donne = gameData[0]
+            pli = gameData[1]
+            joueur = gameData[2]
+            atout = gameData[3]
+            score = gameData[4]
+        
+            return redirect('/game')
+    
+    else: return render_template('load.html')
+   
 @app.route('/initGame')
 def gameStart():
     
@@ -58,8 +91,12 @@ def gameStart():
     else:
         atout = fonctions.getCardColor(card_file,str(donne[-1][-1]['valeur']))
         
-    print(atout)
     return redirect('/game')
+
+@app.route('/download')
+def Download_Save():
+    PATH = 'saves/gameData.bel'
+    return send_file(PATH, as_attachment=True)
 
 if __name__=='__main__':
     app.secret_key = "teobubu"
